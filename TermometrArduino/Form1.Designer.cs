cf5ddd0e40fc93b6f5e8efbdf0a600f93f018f1f
Michał Daniel Domarecki <m.domoarecki@gmail.com>
2018-02-11 01:19:08 +0100

    first commit

---
+++ TermometrArduino/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace TermometrArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comPorList = new System.Windows.Forms.ComboBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // comPorList
+            // 
+            this.comPorList.FormattingEnabled = true;
+            this.comPorList.Location = new System.Drawing.Point(12, 12);
+            this.comPorList.Name = "comPorList";
+            this.comPorList.Size = new System.Drawing.Size(121, 21);
+            this.comPorList.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(549, 96);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(8, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 534);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.comPorList);
+            this.Name = "Form1";
+            this.Text = "Termometr Arduino";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comPorList;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ TermometrArduino/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace TermometrArduino
+{
+    public partial class Form1 : Form
+    {
+
+        private String[] ports;
+        private DialogResult result;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            try
+            {
+                ports = SerialPort.GetPortNames();
+            }
+            catch (Win32Exception e) {
+                MessageBox.Show(e.Message);
+            }
+
+            if (ports.Length == 0) {
+                result =  MessageBox.Show("Nie wykryto portów");
+            }
+
+            if (result == DialogResult.OK) {
+                MessageBox.Show("a");
+                Environment.Exit(1);
+            }
+
+            foreach (String port in ports) {
+                comPorList.Items.Add(new { Text = port, Value = port });
+            }
+
+        }
+    }
+}
